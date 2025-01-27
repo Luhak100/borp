437d8c48b007ebd4baccf94d1b6db07d3f3f4328
Luhak100 <JoshuaRich010@protonmail.com>
2025-01-27 16:42:58 -0600

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Save File Example</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-
 <body>
     <script type="text/javascript">
         $(document).ready(function () {
@@ -33,6 +23,3 @@
             window.URL.revokeObjectURL(url);
         }
     </script>
-</body>
-
-</html>
